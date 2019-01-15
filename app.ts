ddaa3bc106b9bcb66b5048e17e3e1c842212cd50
pzalep1 <43140629+pzalep1@users.noreply.github.com>
2019-01-15 13:16:26 -0500

    Add files via upload
    added a delete route

---
+++ app.ts
@@ -0,0 +1,48 @@
+import { MongoDriverFactory } from "./MongoConnectorFactory";
+//import { disconnect } from "cluster";
+import { MongoDriver } from "./MongoConnector";
+import * as bodyParser from "body-parser"; 
+import * as express from "express"; 
+
+const app = express() 
+const port = 3000
+
+MongoDriverFactory.build()
+  .then(async (datastore) => {
+    /*console.log("Is the datastore a mongodriver?", datastore instanceof MongoDriver); 
+    await datastore.createTask({
+      name: "Homeworkx2"
+    });*/
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+      }),
+    );
+    app.use(bodyParser.json());
+
+    app.get('/', (req, res) => res.send('Hello World!'))
+
+    
+
+    app.post('/tasks', async (req, res) => {
+      const name = req.body.name;
+      const id = await datastore.createTask({
+        name 
+      });
+      res.json ({id});
+    });
+
+    app.delete('/tasks/:id', async (req, res) => {
+      const id = req.params.id; 
+      await datastore.deleteTask(id);
+      return res.sendStatus(200); 
+    });
+
+
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  })
+
+  
+  .catch(e => {
+    throw e;
+  });
